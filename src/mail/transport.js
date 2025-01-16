3ef797685f327b715afacb46c9480da3a05a2452
Ariana <aru.brea@gmail.com>
2025-01-16 10:13:37 -0300

    actualización 2025

--- src/mail/transport.js
+++ src/mail/transport.js
@@ -5,12 +5,13 @@ const { SMTP_HOST, SMTP_PORT, SMTP_PASS, SMTP_USER} = process.env;
 const transport = nodemailer.createTransport({
 
     host: SMTP_HOST,
-    port: SMTP_PORT,
+    port: parseInt(SMTP_PORT,10),
     secure: true,
     auth: {
       user: SMTP_USER,
       pass: SMTP_PASS
-    }
+    },
+    debug: true
   });
 
   module.exports = transport
